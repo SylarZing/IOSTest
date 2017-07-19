7c08be415bf2f91f0857a146d5bbd814a1744768
SylarZing <344847204@qq.com>
2017-07-19 15:19:02 +0800

    Initial Commit

---
+++ IOSTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IOSTest
+//
+//  Created by 崔宗寅 on 2017/7/19.
+//  Copyright © 2017年 崔宗寅. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ IOSTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  IOSTest
+//
+//  Created by 崔宗寅 on 2017/7/19.
+//  Copyright © 2017年 崔宗寅. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
